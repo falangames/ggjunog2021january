389fb43aa1de863a7656466cfe5461b7f5dc44ce
falangames <77401556+falangames@users.noreply.github.com>
2021-01-30 12:51:51 +0300

    begin

---
+++ ggjunog2021january/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask whatIsGround;
+    public float jumpForce;
+    public float speed;
+    Rigidbody2D rb;
+    public float distance = 1.0f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("w") && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(x * speed, rb.velocity.y, 0f);
+        rb.velocity = move;
+    }
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, whatIsGround);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
